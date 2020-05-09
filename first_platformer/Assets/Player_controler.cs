c1005482c76baf6780868b769e7618d1cca3e5ff
Michelle Simoni <michelle.simoni779@gmail.com>
2020-05-09 11:20:18 -0700

    set_up_push
    Coppied over what I made so far into the github repository

---
+++ first_platformer/Assets/Player_controler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_controler : MonoBehaviour
+{
+    public Rigidbody2D player;
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            player.velocity = new Vector2(-2, 0);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            player.velocity = new Vector2(2, 0);
+        }
+    }
+}
