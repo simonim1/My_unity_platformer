1ebc3e723e05955ae978888527cf07fc391c9199
Michelle Simoni <michelle.simoni779@gmail.com>
2020-05-10 20:15:30 -0700

    added a jump thresh hold
    jump thresh hold is now less than 2. so you can only jump once and holding down the key is fixed

--- first_platformer/Assets/Player_controler.cs
+++ first_platformer/Assets/Player_controler.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 public class Player_controler : MonoBehaviour
 {
     public Rigidbody2D player;
+    int jump_thresh_hold = 0;
 
     private void Update()
     {
         if(Input.GetKey(KeyCode.LeftArrow))
         {
             player.velocity = new Vector2(-5, player.velocity.y);
+            jump_thresh_hold = 0;
             //TODO: find a way to change the animation with movement
         }
         if(Input.GetKey(KeyCode.RightArrow))
         {
             player.velocity = new Vector2(5, player.velocity.y);
+            jump_thresh_hold = 0;
             //TODO: change animation
+
         }
-        if(Input.GetKey(KeyCode.UpArrow))
+        if(Input.GetKey(KeyCode.UpArrow) && jump_thresh_hold<2)
         {
-            player.velocity = new Vector2(player.velocity.x, 5);
+
+            //Need to figure out how to limit the up arrow count. 
+            //if user holds the up arrow the character is flying
+            player.velocity = new Vector2(player.velocity.x, 10f);
+            jump_thresh_hold += 1;
         }
     }
 }
